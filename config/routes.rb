7dfecc7982810235a821ebdb6a10669eca6a3369
Evgeny Peleh <pelehev@gmail.com>
2019-03-08 20:53:14 +0600

    Add routes template

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      # resources :votes, only: %i[show create]
+      # resources :posts, only: %i[index show]
+      # resources :subscribers, only: %i[index show create update]
+      # resources :images, only: %i[index show]
+      # resources :images, only: %i[create update], constraints: { subdomain: 'admin' }
+    end
+  end
+
+  # root 'home#index'
+  # get 'weekly-report/:id', to: 'weekly-report#show'
+  # get 'subscribe', to: 'subscribe#index'
+  # get '*path', to: 'home#index'
 end

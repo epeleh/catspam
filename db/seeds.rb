368023e30796933373d9cda5a596fc16a736853e
Evgeny Peleh <pelehev@gmail.com>
2019-03-09 04:11:55 +0600

    Upload startup images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+# Upload startup images
+Rails.root.join('db/seeds/images').glob('*').map do |folder|
+  folder.glob('*').map { |file| { file => folder.basename.to_s.to_i } }
+end.flatten.inject(:merge).each do |img_file, darkness|
+  image = Image.new(darkness: darkness)
+  image.file.attach(io: File.open(img_file), filename: img_file.basename, content_type: "image/#{img_file.extname}")
+  image.save!
+end

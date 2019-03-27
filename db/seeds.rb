1b328b21b3366ff7dacba1905fdbb2c5259dabb4
Evgeny Peleh <epeleh@lineate.com>
2019-03-27 19:56:43 +0600

    Add log for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
-# Upload startup images
+puts '== Upload startup images ======================================================'
+
 Rails.root.join('db/seeds/images').glob('*').map do |folder|
   folder.glob('*').map { |file| { file => folder.basename.to_s.to_i } }
 end.flatten.inject(:merge).each do |img_file, darkness|
+  puts "upload #{img_file}"
   image = Image.new(darkness: darkness)
   image.file.attach(io: File.open(img_file), filename: img_file.basename, content_type: "image/#{img_file.extname}")
   image.save!

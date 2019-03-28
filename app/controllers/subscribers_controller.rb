97fa8e9442ce13377361b7471c84ee6db4f59221
Evgeny Peleh <epeleh@lineate.com>
2019-03-28 21:58:52 +0600

    Update SubscribersController

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -10,8 +10,26 @@ class SubscribersController < ApplicationController
   end
 
   def create
+    @subscriber = Subscriber.new(subscriber_params)
+    if @subscriber.save
+      render json: @subscriber, status: :created
+    else
+      render json: @subscriber.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    @subscriber = Subscriber.find(params[:id])
+    if @subscriber.update(subscriber_params)
+      render json: @subscriber, status: :ok
+    else
+      render json: @subscriber.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def subscriber_params
+    params.permit(:email, :name, :active)
   end
 end

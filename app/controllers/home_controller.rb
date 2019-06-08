8f4245f3d7d10ebcf1c50d79a1a99bf184186944
Evgeny Peleh <pelehev@gmail.com>
2019-06-08 17:32:05 +0600

    Add solo posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@
 class HomeController < ApplicationController
   def index
     @reports = Report.all
-    @solo_posts = Post.active
+    @solo_posts = Post.active.select { |x| x.created_at > Report.pluck(:created_at).max }
   end
 end

ed27365379cb04c59af1fdec800aa8844a98f732
Evgeny Peleh <pelehev@gmail.com>
2019-06-23 17:27:43 +0600

    Fix bug when no reports

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@
 class HomeController < ApplicationController
   def index
     @reports = Report.all
-    @solo_posts = Post.active.select { |x| x.created_at > Report.pluck(:created_at).max }
+    @solo_posts = Post.active.select { |x| x.created_at > (Report.pluck(:created_at).max || 0) }
   end
 end
